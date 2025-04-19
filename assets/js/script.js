efd9d11b3efc2135934316d34c4c4e2db74eabb6
navycrow <hg64off@gmail.com>
2025-04-19 18:23:28 +0200

    optimisation script.js, nettoyage produit.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ function toggleDarkMode() {  // Switch entre le mode dark ou light
     console.log(localStorage)
 }
 
-function ajouterSectionArticles(parent, titre, articles) {
+function ajouterSectionArticles(parent, titre, articles) {  // Articles par catégorie
     // Creation d'une section avec un titre et une div
     const section = document.createElement('section')
     section.classList.add('articles')
@@ -35,58 +35,13 @@ function ajouterSectionArticles(parent, titre, articles) {
     parent.appendChild(section)
 }
 
+function ajouterSectionArticle(parent, article) {  // Fiche produit
+    // Création d'une section article
+    const section = document.createElement('section')
+    section.id = 'article'
 
-
-
-function remplirPageArticles() {  // HOMMES et FEMMES
-    const main = document.querySelector('main')
-
-    // Récupère le groupe de la page (c'est le titre de la bannière)
-    const groupe = document.querySelector('#banniere h1').textContent
-    console.log(groupe)
-
-    // Filtre les articles du groupe
-    const articlesGroupe = articles.filter(article => article.groupe === groupe)
-    console.log(articlesGroupe)
-
-    // Récupère toutes les catégories du groupe
-    let categoriesGroupe = articlesGroupe.map(article => article.categorie)
-    categoriesGroupe = categoriesGroupe.filter((categorie, i) => categoriesGroupe.indexOf(categorie) === i); /* Rend chaque catégorie unique */
-    console.log(categoriesGroupe)
-
-    // Pour chaque catégorie de la page, ajoute la section correspondante
-    categoriesGroupe.forEach(categorie => {
-        const selectionArticles = articlesGroupe.filter(article => article.categorie === categorie)
-        ajouterSectionArticles(main, categorie, selectionArticles)
-
-    //     // Ajout d'une section avec un titre et une div
-    //     const section = document.createElement('section')
-    //     section.classList.add('articles')
-    //     section.innerHTML = `<h2>${categorie}</h2>
-    // <div></div>`
-    //     main.appendChild(section)
-
-    //     // Ajout des articles de la catégorie correspondante à la section dans la div
-    //     const div = section.querySelector('div')
-    //     articlesGroupe.filter(article => article.categorie === categorie).forEach(article => {
-    //         div.innerHTML += `
-    //         <a href="produit.html?id=${article.id}">
-    //             <img src="${article.photo}" alt="${article.nom}" />
-    //             <p>${article.nom}</p>
-    //             <p><strong>${article.prix.toFixed(2)} €</strong></p>
-    //         </a>`
-    //     })
-    })
-}
-
-function remplirPageProduit(id) {
-    // Récupération de la fiche de l'article
-    id = parseInt(id)  // Convertit l'id en type Number
-    const article = articles.find(article => article.id === id)
-    console.log(article)
-
-    const sectionArticle = document.getElementById('article')
-    sectionArticle.innerHTML = `
+    // Ajout des infos de la fiche
+    section.innerHTML = `
         <div id="article-image">
             <img src="${article.photo}" alt="${article.alt}" />
         </div>
@@ -95,7 +50,7 @@ function remplirPageProduit(id) {
             <h2>${article.nom}</h2>
             <p id="prix"><strong>${article.prix.toFixed(2)} €</strong></p>
             <p id="resume">${article.resume}</p>
-            <button onclick="ajouterPanier(${id})">AJOUTER AU PANIER</button>
+            <button onclick="ajouterPanier(${article.id})">AJOUTER AU PANIER</button>
             <section>DESCRIPTION
                 <p>${article.description}</p>
             </section>
@@ -105,11 +60,15 @@ function remplirPageProduit(id) {
             <section>SERVICE-APRES-VENTE
                 <p>${article.sav}</p>
             </section>
-        </div>`
+        </div>
+    `
+
+    // Ajout de la section dans le parent
+    parent.appendChild(section)
 }
 
 function ajouterPanier(id) {
-    alert ('Votre article a bien été ajouté') // Message provisoire
+    alert('Votre article a bien été ajouté') // Message provisoire
 }
 
 // ============================================================================
@@ -123,17 +82,53 @@ if (localStorage.getItem('dark-mode') === 'true') {  /* booléen stocké sous fo
 const url = new URL(window.location)  // https://developer.mozilla.org/en-US/docs/Web/API/URL_API
 console.log(url)
 
-// Si page produit, récupère l'id et remplit la page
+// Si page produit
 if (url.pathname.endsWith('produit.html')) {
-    const articleId = url.searchParams.get('id')  // https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
-    console.log(url.pathname, `id=${articleId}`)
-    remplirPageProduit(articleId)
+    // Récupère l'id de l'article (le convertit en number car get retourne un string)
+    const id = parseInt(url.searchParams.get('id'))  // https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+    console.log(url.pathname, `id=${id}`)
+
+    // Récupération de l'élément main
+    const main = document.querySelector('main')
+  
+    // Ajout de la fiche article corespondante
+    const article = articles.find(article => article.id === id)
+    console.log(article)
+    ajouterSectionArticle(main, article)
+    
+    // Ajout des recommandations (même groupe, même catégorie)
+    const groupe = article.groupe
+    const categorie = article.categorie
+    const selection = articles.filter(article => article.groupe === groupe && article.categorie === categorie)
+    console.log(selection)
+    ajouterSectionArticles(main, 'Vous aimerez aussi', selection)
 }
 
 // Si page homme ou femme, remplit la page avec les articles associés
 else if (url.pathname.endsWith('femme.html') || url.pathname.endsWith('homme.html')) {
     console.log(url.pathname)
-    remplirPageArticles()
+
+    // Récupération de l'élément main
+    const main = document.querySelector('main')
+
+    // Récupère le groupe correspondant à la page (c'est le titre de la bannière)
+    const groupe = document.querySelector('#banniere h1').textContent
+    console.log(groupe)
+
+    // Filtre les articles corespondants au groupe
+    const articlesGroupe = articles.filter(article => article.groupe === groupe)
+    console.log(articlesGroupe)
+
+    // Récupère toutes les catégories du groupe
+    let categoriesGroupe = articlesGroupe.map(article => article.categorie)
+    categoriesGroupe = categoriesGroupe.filter((categorie, i) => categoriesGroupe.indexOf(categorie) === i); /* Rend chaque catégorie unique */
+    console.log(categoriesGroupe)
+
+    // Pour chaque catégorie de la page, ajoute la section correspondante
+    categoriesGroupe.forEach(categorie => {
+        const selectionArticles = articlesGroupe.filter(article => article.categorie === categorie)
+        ajouterSectionArticles(main, categorie, selectionArticles)
+    })
 }
 
 else {
